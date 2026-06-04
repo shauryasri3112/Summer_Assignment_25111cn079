bea27ccb5fd7a3a9801779bc720ffc78d3343c6d
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-04 12:02:30 +0530

    Update Q1.java

---
+++ Day1/Q1.java
@@ -0,0 +1,12 @@
+public class Q1{
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to count the digits:");
+        int num = sc.nextInt();
+        int count = 0;
+        while(num != 0){
+            num = num / 10;
+            count++;
+        }
+    }
+}
