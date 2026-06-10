d469d828e0ca300fb989b5a35826346c46a58892
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-10 12:28:21 +0530

    Create Q2.java

---
+++ Day10/Q2.java
@@ -0,0 +1,19 @@
+public class Q2 {
+    public static void main(String[] args) {
+
+        for (int i = 5; i >= 1; i--) {
+
+            // Print leading spaces
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
