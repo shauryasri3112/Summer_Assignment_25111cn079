e4ad5b3d4fdd29f2138be8539ba231a976ae4abc
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-10 07:29:56 +0530

    Update Q1.java

--- Day1/Q1.java
+++ Day1/Q1.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 public class Q1{
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number to count the digits:");
-        int num = sc.nextInt();
-        int count = 0;
-        while(num != 0){
-            num = num / 10;
-            count++;
-        }
-    }
 
 System.out.println("Enter the number to find the sum of n natural numbers:");
 int n = sc.nextInt();
