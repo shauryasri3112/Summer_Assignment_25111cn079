15a4cd915ac437e0c85e853252d30d322dd9b0cf
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-10 11:31:26 +0530

    Update Q2.java

--- Day 8/Q2.java
+++ Day 8/Q2.java
@@ -1,5 +1,10 @@
-package Day 8;
-
-public class Q2 {
-    
+public class Q2{
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'E'; i++) {
+            for (char j = 'A'; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
