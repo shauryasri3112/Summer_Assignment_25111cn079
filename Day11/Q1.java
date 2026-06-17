d156da2dab58c2fbef96372061ac8eba41c35d0d
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-17 20:42:01 +0530

    Create Q1.java

---
+++ Day11/Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String [] args)
+    {
+        int a=10;
+        int b=20;
+        int c=sum(a,b);
+        System.out.println("Sum of two numbers is: "+c);
+    }
+
+public static int sum (int a, int b) {
+    return a + b;
+}
+}
