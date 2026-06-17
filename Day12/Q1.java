1e8b7611911201a54ef6eed191a165ccf32ed0c9
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-17 20:53:45 +0530

    Create Q1.java

---
+++ Day12/Q1.java
@@ -0,0 +1,21 @@
+public class Q1{
+    public static void main(String[] args) {
+        palindrome(121);
+    }
+    public static void palindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+    }
+}
