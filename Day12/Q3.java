f08ee4c77c58218fde27cf9df11cc937140389fd
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-17 20:54:17 +0530

    Create Q3.java

---
+++ Day12/Q3.java
@@ -0,0 +1,16 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int n=10;
+        fibonacci(n);
+    }
+public static void fibonacci(int n){
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
