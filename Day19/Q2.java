07d931325957bb0f395d13fb5cead6d6f66766c0
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-23 20:27:42 +0530

    Create Q2.java

---
+++ Day19/Q2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[3][3];
+        int[][] b = new int[3][3];
+        int[][] subtract = new int[3][3];
+
+        System.out.println("Enter elements of first matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of second matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Adding matrices
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                subtract[i][j] = a[i][j] - b[i][j];
+            }
+        }
+
+        System.out.println("Subtraction of matrices:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(subtract[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+    
+
