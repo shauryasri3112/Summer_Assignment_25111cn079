90f006d6baf230109dac78f4b8bec95255093470
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-25 21:37:36 +0530

    Create Q1.java

---
+++ Day21/Q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+
+        System.out.println("Length of the string = " + count);
+
+        sc.close();
+    }
+}
