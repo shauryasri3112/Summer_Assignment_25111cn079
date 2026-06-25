894c5f0f40602b40575338d960b8970bbd880c51
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-25 21:38:32 +0530

    Create Q4.java

---
+++ Day21/Q4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String upper = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char)(ch - 32);
+            }
+
+            upper += ch;
+        }
+
+        System.out.println("Uppercase String: " + upper);
+
+        sc.close();
+    }
+}
