ff9c041a632db0a1ed9cc26a3aed9fb7428fbe9b
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-26 22:10:31 +0530

    Create Q4.java

---
+++ Day22/Q4.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                result = result + str.charAt(i);
+            }
+        }
+
+        System.out.println("String after removing spaces: " + result);
+
+        sc.close();
+    }
+}
