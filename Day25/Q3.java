5693f9b4b5ee0fee772038e3733f2d0bb5d0ab4b
shauryasri3112 <shaurya.sri3112@gmail.com>
2026-06-29 18:13:23 +0530

    Create Q3.java

---
+++ Day25/Q3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] names = new String[n];
+
+        System.out.println("Enter the names:");
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (names[j].compareTo(names[j + 1]) > 0) {
+                    String temp = names[j];
+                    names[j] = names[j + 1];
+                    names[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Names in alphabetical order:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(names[i]);
+        }
+
+        sc.close();
+    }
+}
